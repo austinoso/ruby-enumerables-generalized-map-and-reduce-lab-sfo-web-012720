56b20ba0beb15167ec5f440379d15b9304780c06
Austin Osorio <osorio_austin@yahoo.com>
2020-01-17 02:45:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,10 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
-
-  output = yield(array, starting_point)
-
+  i = 0
+  while i < array.length do
+    output = yield(array[i], starting_point)
+    i += 1
+  end
   output
-
 end
